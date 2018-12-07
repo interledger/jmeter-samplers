334b410693f533c50e85007daae29f276765490a
Adrian Hope-Bailie <adrian@hopebailie.com>
2018-12-07 22:54:59 +0200

    Update ci-deploy.sh

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -4,7 +4,7 @@ pushDocker() {
   # Push Docker image tagged latest and tagged with commit descriptor
   local REGISTRY=""
   local NAMESPACE="interledger/"
-  local REPO="jmeter-samplers"
+  local REPO="jmeter-load-test"
   # rm is false because on Circle the process doesn't have permissions to delete the intermediate container
   docker build -t $NAMESPACE$REPO --rm=false .
   docker login -u $DOCKER_USER -p $DOCKER_PASS -e $DOCKER_EMAIL $REGISTRY
